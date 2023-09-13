5ae380996f486f8de0c9007ad124427d99c2e8c8
poudyalamit <poudyalamit20@gmail.com>
2023-09-13 23:57:31 +0545

    added final deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+};
+
+// config for deployment in other test networks
+const URL = process.env.URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+module.exports={
+  solidity: "0.8.19",
+  networks:{
+    mumbai:{
+      url: URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
